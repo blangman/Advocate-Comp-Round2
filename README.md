ae9a047cf27a1541adee33e12ec598088a0ff122
blangman <blangman@college.harvard.edu>
2023-03-29 14:53:34 -0400

    setting up the webapp

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Advocate-Comp-Round2
-
-hi
--- app.py
+++ app.py
@@ -1,19 +1,31 @@
+from flask import Flask, render_template, request
 import openai
 
+app = Flask(__name__)
+
 # Set up API key and endpoint URL
-openai.api_key = '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'
+openai.api_key = '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'
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Get user input from form
+        input_text = request.form['input_text']
 
-# Prompt user for input text
-input_text = input("Enter text to summarize: ")
+        # Call your existing code to generate output
+        output = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": "You are an artist. Write a 15 word sentence that generates a prompt for an AI art piece (including art style, colors, themes, characters, and setting). Here is the story: '/n:" + input_text}
+            ]
+        )
 
+        # Extract the output message from the API response
+        output_message = output.choices[0].text
 
-output = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user","content": "You are an artist. Write one sentence that generates a prompt for an AI art piece (including art style, colors, themes, characters, and setting). Here is the story: '/n:" + input_text}
-    ]
-)
+        return render_template('index.html', output_message=output_message)
+    else:
+        return render_template('index.html')
 
-print(output.choices[0].message)
-
+if __name__ == '__main__':
+    app.run(debug=True)
