e99f039b7d007a112175263ab745308eb66dced2
blangman <blangman@college.harvard.edu>
2023-04-02 15:03:55 -0400

    only one AI art piece generated

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def home():
                 model="gpt-3.5-turbo",
                 messages=[
                     {"role": "user",
-                     "content": "Write a sentence that generates a prompt for an AI art piece based on a story I will share (be sure to mention art style, colors, themes, characters, and setting). Be concise. Here is the story: '/n:" + input1}
+                     "content": "Write a sentence that generates a prompt for an AI art piece based on the first half of a story I will share (be sure to mention art style, colors, themes, characters, and setting). Be concise. Here is the story: '/n:" + input1}
                 ]
             )
 
@@ -55,28 +55,34 @@ def home():
                 model="gpt-3.5-turbo",
                 messages=[
                     {"role": "user",
-                     "content": "Write a sentence that generates a prompt for an AI art piece based on a story I will share (be sure to mention art style, colors, themes, characters, and setting). Be concise. Here is the story: '/n:" + input2}
+                     "content": "Write a sentence that generates a prompt for an AI art piece based on the second half of a story I will share (be sure to mention art style, colors, themes, characters, and setting). Be concise. Here is the story: '/n:" + input2}
                 ]
             )
 
             output_message1 = output1.choices[0].message.content
             output_message2 = output2.choices[0].message.content
 
-            response1 = openai.Image.create(
-                prompt="" + output_message1,
+            output3 = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "user",
+                     "content": "Merge these two prompts for an AI art piece into one prompt that pulls from elements of both. Here is the first prompt:" + output_message1 + "Here is the second prompt:" + output_message2 + "keep it concise."}
+                ]
+            )
+
+            output_message3 = output3.choices[0].message.content
+
+            
+
+            output = openai.Image.create(
+                prompt="" + output_message3,
                 n=1,
                 size="512x512"
             )
 
-            response2 = openai.Image.create(
-                prompt="" + output_message2,
-                n=1,
-                size="512x512"
-            )
+            image_url1 = output['data'][0]['url']
 
-            image_url1 = response1['data'][0]['url']
-            image_url2 = response2['data'][0]['url']
-            return render_template('index.html', output_message1=output_message1, output_message2=output_message2, image_url1=image_url1, image_url2=image_url2)
+            return render_template('index.html',  image_url1=image_url1, output_message3=output_message3)
         else:
             print("Input must be under 30,000 characters. Input is currently " + str(len(input_text)) + " characters")
             return render_template('index.html')
